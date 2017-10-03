fe3ffd2724b201c87d84cb55ff049de8248b1951
arrizaqu <arrizaqu@yahoo.com>
2017-10-03 13:45:19 +0700

    update script

--- CustomeSegitiga.java
+++ CustomeSegitiga.java
@@ -1,4 +1,4 @@
-package com.java.loglic.custome;
+package com.java.logic.day1;
 
 public class CustomeSegitiga {
 
@@ -6,22 +6,34 @@ public class CustomeSegitiga {
 	private int dimY = 0;
 	private String[][] data;
 	
-	public void setDataArray(int input){
+	public void setCanvas(int input){
 		this.dimX = input;
-		this.dimY = input;
+		this.dimY = input/ 2 + 1;
 		
 		data = new String[dimY][dimX];
 		for(int i = 0; i < dimY; i++){
 			for(int j = 0; j < dimX; j++){
-				if(i == j){
-					this.data[i][j] = "*";
+				if(i == 0 && j == input / 2){
+					getPola(data, input);
 				}
 			}
 		}
 	}
 	
+	public String[][] getPola(String[][] data, int n){
+		for(int bangun = 0; bangun < n / 2 + 1; bangun++){
+			for(int i = 0+ bangun; i < 1+bangun; i++){
+				for(int j = (n / 2) -bangun; j < (n / 2 + 1)+bangun; j++  ){
+					data[i][j] = "R";
+				}
+			}
+		}
+		
+		return data;
+	}
+	
 	public void showDataArray(int input){
-		setDataArray(input);
+		setCanvas(input);
 		
 		for(int i = 0; i < dimY; i++){
 			for(int j = 0; j < dimX; j++){
@@ -33,6 +45,11 @@ public class CustomeSegitiga {
 	
 	public static void main(String args[]){
 		CustomeSegitiga soal = new CustomeSegitiga();
-		soal.showDataArray(7);
+		try{
+			soal.showDataArray(7);
+		}catch(Exception e){
+			System.out.println("harus bilangan ganjil");
+		}
+		
 	}
 }
